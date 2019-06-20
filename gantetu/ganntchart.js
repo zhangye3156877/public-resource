c90239fd12f6d200635d99bf9f1ebf9ac05f3d1f
zhangchi <624593758@qq.com>
2019-06-20 18:32:42 +0800

    22

--- gantetu/ganntchart.js
+++ gantetu/ganntchart.js
@@ -27,7 +27,7 @@ charts.GanttChart.prototype.init = function (options = {}) {
   const top = scollDom.scrollTop();
   const width = options.axis.axisXSize || 0;
   const height = options.axis.axisYSize || 0;
-  
+
   $('#scroll-mask').css({
     width: `${this.paddingLeft + this.paddingRight + width}px`,
     height: `${this.paddingTop + this.paddingBottom + height}px`,
@@ -37,7 +37,7 @@ charts.GanttChart.prototype.init = function (options = {}) {
     height: `${this.height}px`
   });
   $('#container').scroll(this.scoll.bind(this));
-  
+
   this.stage = new Konva.Stage({
     container: this.container,
     width: this.width,
@@ -58,7 +58,7 @@ charts.GanttChart.prototype.init = function (options = {}) {
 }
 
 // 滚动方法
-charts.GanttChart.prototype.scoll = function(e){
+charts.GanttChart.prototype.scoll = function (e) {
   const scollDom = this.scollDom;
   const canvasWrapDom = this.canvasWrapDom;
   const left = scollDom.scrollLeft();
@@ -73,7 +73,7 @@ charts.GanttChart.prototype.scoll = function(e){
   })
   this.layer.draw();
 }
-// 坐标轴类
+// 轴线格局类
 charts.Axis = function (options = {}) {
   this.paddingLeft = options.paddingLeft || 0;
   this.paddingRight = options.paddingRight || 0;
@@ -86,24 +86,29 @@ charts.Axis = function (options = {}) {
   this.axisYSize = options.axisYSize || 0;
   this.tickSize = options.tickSize || 10;
   this.tickXInterval = options.tickXInterval || 200;
-  this.tickYInterval = options.tickYInterval || 150;
+  this.tickYInterval = options.tickYInterval || 100;
   this.tickXChidren = options.tickXChidren || 4;
   this.tickChidrenSize = options.tickChidrenSize || 5;
+  this.gridColor = options.gridColor || '#d9d9d9';
+  this.gridStrokeWidth = options.gridStrokeWidth || 1;
   this.axisX = null;
   this.axisY = null;
   this.tickX = null;
   this.tickY = null;
-  //this.tickText = null;
+  this.axisXText = null;
+  this.axisYText = null;
+  this.gridX = null;
+  this.gridY = null;
   this.group = new Konva.Group();
   this.init();
 };
 charts.Axis.prototype.init = function () {
 
-  function getBytesLength (str) {
+  function getBytesLength(str) {
     let num = str.length;
-    for (let i = 0; i < str.length; i ++) { 
-      if (str.charCodeAt(i) > 255) { 
-        num ++;
+    for (let i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) > 255) {
+        num++;
       }
     }
     return num;
@@ -123,8 +128,10 @@ charts.Axis.prototype.init = function () {
   });
   this.axisXText = new Konva.Group();
   this.axisYText = new Konva.Group();
+  this.gridX = new Konva.Group();
+  this.gridY = new Konva.Group();
   let pathTicksForAxis = '';
-  for (let i = 1; i < this.axisXSize / this.tickXInterval; i++) {
+  for (let i = 1; i <= this.axisXSize / this.tickXInterval; i++) {
     pathTicksForAxis += `M${this.tickXInterval * i},0L${this.tickXInterval * i},${-this.tickSize}`;
     const str = `横轴${i}`;
     const num = getBytesLength(str);
@@ -134,22 +141,38 @@ charts.Axis.prototype.init = function () {
       fontSize: this.fontSize,
       fontFamily: 'Calibri',
       fill: this.axisColor
-    })
+    });
+    const line = new Konva.Line({
+      points: [this.paddingLeft + this.tickXInterval * i, this.paddingTop, this.paddingLeft + this.tickXInterval * i, this.paddingTop + this.axisYSize],
+      stroke: this.gridColor,
+      lineCap: 'round',
+      strokeWidth: this.gridStrokeWidth,
+    });
     this.axisXText.add(text);
+    this.gridY.add(line);
   }
-  for (let i = 1; i < this.axisYSize / this.tickYInterval; i++) {
-    pathTicksForAxis += `M0,${this.tickYInterval * i}L${-this.tickSize},${this.tickYInterval * i}`;
+  for (let i = 1; i <= this.axisYSize / this.tickYInterval; i++) {
+    // pathTicksForAxis += `M0,${this.tickYInterval * i}L${-this.tickSize},${this.tickYInterval * i}`;
     const str = `纵轴${i}`;
     const num = getBytesLength(str);
+
     const text = new Konva.Text({
       x: -this.fontSize * num / 2 - this.tickSize - 3,
-      y: this.tickYInterval * i - this.fontSize / 2,
+      y: this.tickYInterval * (i - .5),
       text: str,
       fontSize: this.fontSize,
       fontFamily: 'Calibri',
       fill: this.axisColor,
-    })
+    });
+    const line = new Konva.Line({
+      points: [this.paddingLeft , this.paddingTop + this.tickYInterval * i, this.paddingLeft + this.axisXSize, this.paddingTop + this.tickYInterval * i],
+      stroke: this.gridColor,
+      lineCap: 'round',
+      strokeWidth: this.gridStrokeWidth,
+    });
+    console.log(line)
     this.axisYText.add(text);
+    this.gridX.add(line)
   }
 
   this.tickX = new Konva.Path({
@@ -166,5 +189,5 @@ charts.Axis.prototype.init = function () {
     x: this.paddingLeft,
     y: this.paddingTop,
   })
-  this.group.add(this.axisX, this.axisY, this.tickX, this.axisXText, this.axisYText);
+  this.group.add(this.axisX, this.axisY, this.tickX, this.axisXText, this.axisYText, this.gridX, this.gridY);
 };
