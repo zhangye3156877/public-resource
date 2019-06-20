ce33792b118d49cc6e875440511a4ddfaba9f9cc
zhangchi <624593758@qq.com>
2019-06-20 16:22:14 +0800

    z

---
+++ gantetu/ganntchart.js
@@ -0,0 +1,170 @@
+// all
+var charts = {
+  GanttChart: null,
+  Axis: null,
+}
+// 甘特图主体
+charts.GanttChart = function (options = {}) {
+  this.container = options.canvas;
+  this.scollDom = null;
+  this.canvasWrapDom = null;
+  this.stage = null;
+  this.layer = null;
+  this.width = options.width || window.innerWidth;
+  this.height = options.height || window.innerHeight;
+  this.paddingLeft = options.paddingLeft || 0;
+  this.paddingRight = options.paddingRight || 0;
+  this.paddingTop = options.paddingTop || 0;
+  this.paddingBottom = options.paddingBottom || 0;
+  this.axis = null;
+}
+charts.GanttChart.prototype.init = function (options = {}) {
+  //固定样式
+  const scollDom = $(`#container`);
+  this.scollDom = scollDom;
+  this.canvasWrapDom = $(`#${this.container}`);
+  const left = scollDom.scrollLeft();
+  const top = scollDom.scrollTop();
+  const width = options.axis.axisXSize || 0;
+  const height = options.axis.axisYSize || 0;
+  
+  $('#scroll-mask').css({
+    width: `${this.paddingLeft + this.paddingRight + width}px`,
+    height: `${this.paddingTop + this.paddingBottom + height}px`,
+  });
+  $(`#${this.container}`).css({
+    width: `${this.width}px`,
+    height: `${this.height}px`
+  });
+  $('#container').scroll(this.scoll.bind(this));
+  
+  this.stage = new Konva.Stage({
+    container: this.container,
+    width: this.width,
+    height: this.height
+  });
+  this.layer = new Konva.Layer();
+  if (options.axis !== false) {
+    let ap = options.axis || {};
+    this.axis = new charts.Axis(Object.assign(ap, {
+      paddingLeft: this.paddingLeft,
+      paddingRight: this.paddingRight,
+      paddingTop: this.paddingTop,
+      paddingBottom: this.paddingBottom
+    }));
+    this.layer.add(this.axis.group);
+  }
+  this.stage.add(this.layer);
+}
+
+// 滚动方法
+charts.GanttChart.prototype.scoll = function(e){
+  const scollDom = this.scollDom;
+  const canvasWrapDom = this.canvasWrapDom;
+  const left = scollDom.scrollLeft();
+  const top = scollDom.scrollTop();
+  console.log(left, top);
+  canvasWrapDom.css({
+    transform: `translate3d(${left}px, ${top}px, 0)`
+  });
+  this.layer.setAttrs({
+    x: -left,
+    y: -top
+  })
+  this.layer.draw();
+}
+// 坐标轴类
+charts.Axis = function (options = {}) {
+  this.paddingLeft = options.paddingLeft || 0;
+  this.paddingRight = options.paddingRight || 0;
+  this.paddingTop = options.paddingTop || 0;
+  this.paddingBottom = options.paddingBottom || 0;
+  this.strokeWidth = options.strokeWidth || 2;
+  this.fontSize = options.fontSize || 15;
+  this.axisColor = options.colors || '#d9d9d9';
+  this.axisXSize = options.axisXSize || 0;
+  this.axisYSize = options.axisYSize || 0;
+  this.tickSize = options.tickSize || 10;
+  this.tickXInterval = options.tickXInterval || 200;
+  this.tickYInterval = options.tickYInterval || 150;
+  this.tickXChidren = options.tickXChidren || 4;
+  this.tickChidrenSize = options.tickChidrenSize || 5;
+  this.axisX = null;
+  this.axisY = null;
+  this.tickX = null;
+  this.tickY = null;
+  //this.tickText = null;
+  this.group = new Konva.Group();
+  this.init();
+};
+charts.Axis.prototype.init = function () {
+
+  function getBytesLength (str) {
+    let num = str.length;
+    for (let i = 0; i < str.length; i ++) { 
+      if (str.charCodeAt(i) > 255) { 
+        num ++;
+      }
+    }
+    return num;
+  }
+
+  this.axisX = new Konva.Line({
+    points: [this.paddingLeft, this.paddingTop, this.paddingLeft + this.axisXSize, this.paddingTop],
+    stroke: this.axisColor,
+    lineCap: 'round',
+    strokeWidth: this.strokeWidth,
+  });
+  this.axisY = new Konva.Line({
+    points: [this.paddingLeft, this.paddingTop, this.paddingLeft, this.paddingTop + this.axisYSize],
+    stroke: this.axisColor,
+    lineCap: 'round',
+    strokeWidth: this.strokeWidth,
+  });
+  this.axisXText = new Konva.Group();
+  this.axisYText = new Konva.Group();
+  let pathTicksForAxis = '';
+  for (let i = 1; i < this.axisXSize / this.tickXInterval; i++) {
+    pathTicksForAxis += `M${this.tickXInterval * i},0L${this.tickXInterval * i},${-this.tickSize}`;
+    const str = `横轴${i}`;
+    const num = getBytesLength(str);
+    const text = new Konva.Text({
+      x: this.tickXInterval * i - this.fontSize * num / 4,
+      text: str,
+      fontSize: this.fontSize,
+      fontFamily: 'Calibri',
+      fill: this.axisColor
+    })
+    this.axisXText.add(text);
+  }
+  for (let i = 1; i < this.axisYSize / this.tickYInterval; i++) {
+    pathTicksForAxis += `M0,${this.tickYInterval * i}L${-this.tickSize},${this.tickYInterval * i}`;
+    const str = `纵轴${i}`;
+    const num = getBytesLength(str);
+    const text = new Konva.Text({
+      x: -this.fontSize * num / 2 - this.tickSize - 3,
+      y: this.tickYInterval * i - this.fontSize / 2,
+      text: str,
+      fontSize: this.fontSize,
+      fontFamily: 'Calibri',
+      fill: this.axisColor,
+    })
+    this.axisYText.add(text);
+  }
+
+  this.tickX = new Konva.Path({
+    x: this.paddingLeft,
+    y: this.paddingTop,
+    data: pathTicksForAxis,
+    stroke: this.axisColor,
+  });
+  this.axisXText.setAttrs({
+    x: this.paddingLeft,
+    y: this.paddingTop - this.tickSize - this.fontSize
+  });
+  this.axisYText.setAttrs({
+    x: this.paddingLeft,
+    y: this.paddingTop,
+  })
+  this.group.add(this.axisX, this.axisY, this.tickX, this.axisXText, this.axisYText);
+};
