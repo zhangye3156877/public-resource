d37e44c139e9f882d31bccae916b921f5a9bf612
zhangchi <624593758@qq.com>
2020-05-18 15:19:21 +0800

    fangfangyouse

---
+++ nanfangyouse/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Button} from 'antd';
+import styles from './index.css';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+        <li>
+          <Button type="danger">
+            start
+          </Button>
+        </li>
+      </ul>
+    </div>
+  );
+}
