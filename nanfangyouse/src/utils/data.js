d64a5dbfb9ff562b061d9a830e67b02dcf7a291c
zhangchi <624593758@qq.com>
2020-06-18 17:47:22 +0800

    提取colomuns

---
+++ nanfangyouse/src/utils/data.js
@@ -0,0 +1,368 @@
+export const list1_1 = [
+  {
+    index: 0,
+    required: false,
+    delete: false,
+    clean: false,
+    name: '水星轮',
+    number: 10001,
+    Cu: 0,
+    Fe: 0,
+    S: 0,
+    SiO2: 0,
+    CaO: 0,
+    As: 0,
+    Zn: 0,
+    Pb: 0,
+    MgO: 0,
+    Al2O3: 0,
+    H2O: 0,
+    Sb: 0,
+    Ni: 0,
+    Bi: 0,
+    Ag: 0,
+    Au: 0,
+    inventory: 1000,
+    calculatePercentage: 0,
+    inventoryBalance: ''
+  },
+  {
+    index: 0,
+    required: false,
+    delete: false,
+    clean: false,
+    name: '莱科塔',
+    number: 10002,
+    Cu: 0,
+    Fe: 0,
+    S: 0,
+    SiO2: 0,
+    CaO: 0,
+    As: 0,
+    Zn: 0,
+    Pb: 0,
+    MgO: 0,
+    Al2O3: 0,
+    H2O: 0,
+    Sb: 0,
+    Ni: 0,
+    Bi: 0,
+    Ag: 0,
+    Au: 0,
+    inventory: 1000,
+    calculatePercentage: 0,
+    inventoryBalance: ''
+  },
+  {
+    index: 2,
+    required: false,
+    delete: false,
+    clean: false,
+    name: '和盛',
+    number: 10003,
+    Cu: 0,
+    Fe: 0,
+    S: 0,
+    SiO2: 0,
+    CaO: 0,
+    As: 0,
+    Zn: 0,
+    Pb: 0,
+    MgO: 0,
+    Al2O3: 0,
+    H2O: 0,
+    Sb: 0,
+    Ni: 0,
+    Bi: 0,
+    Ag: 0,
+    Au: 0,
+    inventory: 1000,
+    calculatePercentage: 0,
+    inventoryBalance: ''
+  },
+  {
+    index: 3,
+    required: false,
+    delete: false,
+    clean: false,
+    name: '方舟21',
+    number: 10004,
+    Cu: 0,
+    Fe: 0,
+    S: 0,
+    SiO2: 0,
+    CaO: 0,
+    As: 0,
+    Zn: 0,
+    Pb: 0,
+    MgO: 0,
+    Al2O3: 0,
+    H2O: 0,
+    Sb: 0,
+    Ni: 0,
+    Bi: 0,
+    Ag: 0,
+    Au: 0,
+    inventory: 1000,
+    calculatePercentage: 0,
+    inventoryBalance: ''
+  },
+  {
+    index: 4,
+    required: false,
+    delete: false,
+    clean: false,
+    name: '江门商人',
+    number: 10005,
+    Cu: 0,
+    Fe: 0,
+    S: 0,
+    SiO2: 0,
+    CaO: 0,
+    As: 0,
+    Zn: 0,
+    Pb: 0,
+    MgO: 0,
+    Al2O3: 0,
+    H2O: 0,
+    Sb: 0,
+    Ni: 0,
+    Bi: 0,
+    Ag: 0,
+    Au: 0,
+    inventory: 1000,
+    calculatePercentage: 0,
+    inventoryBalance: ''
+  },
+]
+
+export const columns1_1 = [
+  {
+    title: '库存物料',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Cu',
+    dataIndex: 'Cu',
+  },
+  {
+    title: 'Fe',
+    dataIndex: 'Fe',
+  },
+  {
+    title: 'S',
+    dataIndex: 'S',
+  },
+  {
+    title: 'SiO2',
+    dataIndex: 'SiO2',
+  }, {
+    title: 'CaO',
+    dataIndex: 'CaO',
+  },
+  {
+    title: 'As',
+    dataIndex: 'As',
+  }, {
+    title: 'Zn',
+    dataIndex: 'Zn',
+  },
+  {
+    title: 'Pb',
+    dataIndex: 'Pb',
+  },
+  {
+    title: 'MgO',
+    dataIndex: 'MgO',
+  },
+  {
+    title: 'Al2O3',
+    dataIndex: 'Al2O3',
+  },
+  {
+    title: 'H2O',
+    dataIndex: 'H2O',
+  },
+  {
+    title: 'Sb',
+    dataIndex: 'Sb',
+  },
+  {
+    title: 'Bi',
+    dataIndex: 'Bi',
+  },
+  {
+    title: 'Ni',
+    dataIndex: 'Ni',
+  },
+  {
+    title: 'Ag(t/g)',
+    dataIndex: 'Ag',
+  },
+  {
+    title: 'Au(t/g)',
+    dataIndex: 'Au',
+  }
+]
+
+export const columns1_2 = [
+  {
+    title: '名称',
+    dataIndex: 'name',
+  },
+  {
+    title: '批次号',
+    dataIndex: 'number',
+  },
+  {
+    title: 'Cu',
+    dataIndex: 'Cu',
+  },
+  {
+    title: 'Fe',
+    dataIndex: 'Fe',
+  },
+  {
+    title: 'S',
+    dataIndex: 'S',
+  },
+  {
+    title: 'SiO2',
+    dataIndex: 'SiO2',
+  }, {
+    title: 'CaO',
+    dataIndex: 'CaO',
+  },
+  {
+    title: 'As',
+    dataIndex: 'As',
+  }, {
+    title: 'Zn',
+    dataIndex: 'Zn',
+  },
+  {
+    title: 'Pb',
+    dataIndex: 'Pb',
+  },
+  {
+    title: 'MgO',
+    dataIndex: 'MgO',
+  },
+  {
+    title: 'Al2O3',
+    dataIndex: 'Al2O3',
+  },
+  {
+    title: 'H2O',
+    dataIndex: 'H2O',
+  },
+  {
+    title: 'Sb',
+    dataIndex: 'Sb',
+  },
+  {
+    title: 'Bi',
+    dataIndex: 'Bi',
+  },
+  {
+    title: 'Ni',
+    dataIndex: 'Ni',
+  },
+  {
+    title: 'Ag(t/g)',
+    dataIndex: 'Ag',
+  },
+  {
+    title: 'Au(t/g)',
+    dataIndex: 'Au',
+  },
+  {
+    title: '库存/吨',
+    dataIndex: 'inventory',
+  },
+  {
+    title: '演算比例',
+    dataIndex: 'calculatePercentage',
+    // editable: true
+  },
+  {
+    title: '库存余量',
+    dataIndex: 'inventoryBalance',
+  },
+  {
+    title: '调整比例',
+    dataIndex: 'adjustRatio',
+    editable: true
+  },
+];
+
+export const columns1_3 = [
+  {
+    title: '入炉混合料',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Cu',
+    dataIndex: 'Cu',
+  },
+  {
+    title: 'Fe',
+    dataIndex: 'Fe',
+  },
+  {
+    title: 'S',
+    dataIndex: 'S',
+  },
+  {
+    title: 'SiO2',
+    dataIndex: 'SiO2',
+  }, {
+    title: 'CaO',
+    dataIndex: 'CaO',
+  },
+  {
+    title: 'As',
+    dataIndex: 'As',
+  }, {
+    title: 'Zn',
+    dataIndex: 'Zn',
+  },
+  {
+    title: 'Pb',
+    dataIndex: 'Pb',
+  },
+  {
+    title: 'MgO',
+    dataIndex: 'MgO',
+  },
+  {
+    title: 'Al2O3',
+    dataIndex: 'Al2O3',
+  },
+  {
+    title: 'H2O',
+    dataIndex: 'H2O',
+  },
+  {
+    title: 'Sb',
+    dataIndex: 'Sb',
+  },
+  {
+    title: 'Bi',
+    dataIndex: 'Bi',
+  },
+  {
+    title: 'Ni',
+    dataIndex: 'Ni',
+  },
+  {
+    title: 'Ag(t/g)',
+    dataIndex: 'Ag',
+  },
+  {
+    title: 'Au(t/g)',
+    dataIndex: 'Au',
+  },
+]
+
+
