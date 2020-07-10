ce1f6bf72dd846aa0eceeeffb118dbca6e353bf2
zhangchi <6245937458@qq.com>
2020-07-10 12:41:22 +0800

    添加导出，部分

--- nanfangyouse/src/pages/page3/index.js
+++ nanfangyouse/src/pages/page3/index.js
@@ -17,21 +17,33 @@ import {
   Space,
   Upload
 } from 'antd';
-import XLSX  from 'xlsx';
+import XLSX from 'xlsx';
 //import { UploadOutlined } from '@ant-design/icons';
 import { connect } from 'dva';
+import {workbook2blob, openDownloadDialog} from '@/utils/fn';
 import { columns3_1 } from '@/utils/data';
 import request from '@/utils/request';
 import styles from '../index.less';
 import selfStyle from './index.less';
 
-function customRequest (e){
+function customRequest(e) {
   const reader = new FileReader();
-  reader.onload = function(e) {
+  reader.onload = function (e) {
     const data = e.target.result;
-    const xlsxData = XLSX.read(data, {type:'binary'});
+    const xlsxData = XLSX.read(data, { type: 'binary' });
     const value = XLSX.utils.sheet_to_json(xlsxData.Sheets[xlsxData.SheetNames[0]]);
+    XLSX.writeFile(data, 'aaf');
     console.log(value)
+
+    let sheet1data = [{ department: "行政部", count: 2 }, { department: "前端部", count: 2 }];
+    var sheet1 = XLSX.utils.json_to_sheet(sheet1data);
+
+    /* create a new blank workbook */
+    //var wb = XLSX.utils.book_new();
+    //XLSX.utils.book_append_sheet(wb, sheet1, "部门统计");
+    //const workbookBlob = workbook2blob(wb);
+    //openDownloadDialog(workbookBlob, `部门统计.xlsx`);
+
   }
   reader.readAsBinaryString(e.file);
 }
